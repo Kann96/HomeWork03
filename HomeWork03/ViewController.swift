d452d87ae2d999ce4c1175b242cb8fbb51a83c8c
Kann96 <kann9620@gmail.com>
2024-07-03 17:02:15 +0300

    Initial Commit

---
+++ HomeWork03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork03
+//
+//  Created by Анастасия Кучер on 03.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
